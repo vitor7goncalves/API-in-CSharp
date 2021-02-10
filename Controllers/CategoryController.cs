9210ef740ac252bd9064dddd6aefaad3dd6d8ac7
Vitor Gonçalves Rodrigues <vitor7goncalves@outlook.com>
2021-02-10 19:26:46 -0300

    first commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using testeef.Models;
+using testeef.Data;
+
+namespace testeef.Controllers{
+    [ApiController]
+    [Route("v1/categories")]
+
+    public class categoryController : ControllerBase{
+        [HttpGet]
+        [Route("")]
+
+        public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context){
+            var categories = await context.Categories.ToListAsync();
+            return categories;
+        }
+
+        [HttpPost]
+        [Route("")]
+
+        public async Task<ActionResult<Category>> Post(
+            [FromServices] DataContext context,
+            [FromBody] Category model){
+            if(ModelState.IsValid){
+                context.Categories.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }else{
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
